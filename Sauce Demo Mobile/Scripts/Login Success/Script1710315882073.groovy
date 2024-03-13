cd58cc3df38946c7f0c5ea677757ad668e113de6
yuhan <yuhan.adit@gmail.com>
2024-03-13 15:21:51 +0700

    Test Automation Katalon Mobile

---
+++ Sauce Demo Mobile/Scripts/Login Success/Script1710315882073.groovy
@@ -0,0 +1,43 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startExistingApplication('com.saucelabs.mydemoapp.rn')
+
+Mobile.tap(findTestObject('Icon Hamburger'), 0)
+
+Mobile.tap(findTestObject('Menu Log Out'), 0)
+
+Mobile.tap(findTestObject('Confirmation Logout Button'), 0)
+
+Mobile.tap(findTestObject('Button OK Success Logout'), 0)
+
+Mobile.tap(findTestObject('Icon Hamburger'), 0)
+
+Mobile.tap(findTestObject('Object Repository/Menu Log In'), 0)
+
+Mobile.setText(findTestObject('Object Repository/Input Username'), 'bob@example.com', 0)
+
+Mobile.setText(findTestObject('Object Repository/Input Password'), '10203040', 0)
+
+Mobile.tap(findTestObject('Object Repository/Button Login'), 0)
+
+Mobile.verifyElementText(findTestObject('Object Repository/Title Products'), 'Products')
+
+Mobile.closeApplication()
+
